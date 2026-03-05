30dc04811106e4b40fd40d0034fa590be6cbbb84
sabbanisiddarth <sabbanisiddartha843@gmail.com>
2026-03-05 16:10:57 +0530

    Add initial home page component

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Happy Birthday! 🎉</h1>
+      <p>Wishing you a wonderful day filled with joy and happiness!</p>
+    </div>
+  );
+}
